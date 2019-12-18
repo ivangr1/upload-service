e90cb93cdb6b8c40c3b407f2063ebed9a94c749f
Ivan <grubi.ivan@gmail.com>
2019-12-18 18:50:33 +0100

    Initial commit

---
+++ src/main/java/com/infobip/uploadservice/Configuration.java
@@ -0,0 +1,27 @@
+package com.infobip.uploadservice;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@org.springframework.context.annotation.Configuration
+@EnableAsync
+public class Configuration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Configuration.class);
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        LOGGER.debug("Creating Async Task Executor");
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(8);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("UploadThread-");
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ src/main/java/com/infobip/uploadservice/UploadController.java
@@ -0,0 +1,75 @@
+package com.infobip.uploadservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.Executor;
+
+
+@RestController
+@RequestMapping("/api/v1/upload")
+public class UploadController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+
+    private Executor executor;
+    private Map<String, Long> duration = new HashMap<>();
+
+    @Autowired
+    public UploadController(Executor executor) { this.executor = executor; }
+
+    @PostMapping
+    public @ResponseBody ResponseEntity uploadFiles(@RequestParam("files") MultipartFile[] files, final HttpServletRequest httpServletRequest) {
+        String fileName = httpServletRequest.getHeader("X-Upload-File");
+        final long start = System.currentTimeMillis();
+        Set<String> fileNames = new HashSet<>();
+        try {
+            for (final MultipartFile file : files) {
+                String filename = StringUtils.cleanPath(file.getOriginalFilename());
+                if(fileNames.contains(fileName))
+                    continue;
+                else
+                    fileNames.add(filename);
+                executor.execute(() -> {
+                    try {
+                        byte[] bytes = file.getBytes();
+                        Path path = Paths.get("upload-dir").resolve(filename);
+                        Files.write(path, bytes);
+                        long timestamp = System.currentTimeMillis();
+                        duration.put(String.format("%s-%d", filename, timestamp), (timestamp - start));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/progress")
+    public ResponseEntity uploadProgress() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/duration")
+    public ResponseEntity<List<String>> uploadDuration() {
+        List<String> duration = new ArrayList<>();
+        this.duration.forEach((filename, dur) -> duration.add(String.format("upload_duration{id=”%s”} %d", filename, dur)));
+
+        return ResponseEntity.status(HttpStatus.OK).body(duration);
+    }
+}
