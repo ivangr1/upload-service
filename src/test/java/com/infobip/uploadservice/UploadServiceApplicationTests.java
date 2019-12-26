dbc7db6ec01cdb68bfddd1c5b366f556771a3e7b
Ivan <grubi.ivan@gmail.com>
2019-12-26 17:15:48 +0100

    Initial commit

--- src/test/java/com/infobip/uploadservice/UploadServiceApplicationTests.java
+++ src/test/java/com/infobip/uploadservice/UploadServiceApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
@@ -32,7 +33,7 @@ class UploadServiceApplicationTests {
 
 		List<HttpUploadRequest> mvcResults = new ArrayList<>();
 		for(int i = 0; i < fileNumber; i++) {
-			HttpUploadRequest task = new HttpUploadRequest();
+			HttpUploadRequest task = new HttpUploadRequest(i);
 			mvcResults.add(task);
 		}
 
@@ -40,18 +41,23 @@ class UploadServiceApplicationTests {
 
 		for(Future<MvcResult> resultFuture : mvcResultsFutures) {
 			MvcResult result = resultFuture.get();
-			assertEquals(result.getResponse().getStatus(), 201);
+			assertEquals(HttpStatus.CREATED.value(), result.getResponse().getStatus());
 		}
 	}
 
 	public class HttpUploadRequest implements Callable<MvcResult> {
 		int fileSize = 50 * 1024 * 1024;
+		int i;
+
+		HttpUploadRequest(int i) {
+			this.i = i;
+		}
 
 		public MvcResult call() throws Exception {
 			MockMultipartHttpServletRequestBuilder mockMvcRequestBuilders = MockMvcRequestBuilders.multipart("/api/v1/upload");
 			mockMvcRequestBuilders.header("Content-Length", fileSize);
-			mockMvcRequestBuilders.file("test" + System.currentTimeMillis() + ".zip", new byte[fileSize]).contentType("multipart/form-data; boundary=Boundary");
-			mockMvcRequestBuilders.header("X-Upload-File", "test" + System.currentTimeMillis() + ".zip");
+			mockMvcRequestBuilders.file("test" + this.i + ".zip", new byte[fileSize]).contentType("multipart/form-data; boundary=Boundary");
+			mockMvcRequestBuilders.header("X-Upload-File", "test" + this.i + ".zip");
 			return mockMvc.perform(mockMvcRequestBuilders).andReturn();
 		}
 	}
